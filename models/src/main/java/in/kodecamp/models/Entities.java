8e054ac2fcbe2fa89b84bb468de694ac05f52bae
kodecamp <kodecamp.in@gmail.com>
2019-11-13 16:16:01 +0530

    container for list of entities

---
+++ models/src/main/java/in/kodecamp/models/Entities.java
@@ -0,0 +1,27 @@
+package in.kodecamp.models;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public final class Entities<T> {
+
+  private Collection<T> items;
+
+  public Entities(Collection<T> entities) {
+    items = entities;
+  }
+
+  // default constructor
+  public Entities() {
+
+  }
+
+  public Collection<T> getItems() {
+    return Collections.unmodifiableCollection(this.items);
+  }
+
+  public void setItems(final Collection<T> newItems) {
+    this.items = newItems;
+  }
+
+}
