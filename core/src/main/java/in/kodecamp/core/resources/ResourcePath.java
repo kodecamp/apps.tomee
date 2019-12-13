ea2c25004d6b213008baf40785975fdd1de01dff
kodecamp <kodecamp.in@gmail.com>
2019-12-13 18:10:26 +0530

    Updated

--- core/src/main/java/in/kodecamp/core/resources/ResourcePath.java
+++ core/src/main/java/in/kodecamp/core/resources/ResourcePath.java
@@ -9,7 +9,6 @@ import java.io.*;
 
 public final class ResourcePath {
 
-  // ResourcePath(Link.STUDENTS, StudentEntity.class);
   private static final String DEFAULT_MAPPING_FILE="entitypath-mappings";
   private static  Map<String,String> entityPathMap = new HashMap<>();
 
@@ -38,35 +37,10 @@ public final class ResourcePath {
     }
   }
 
-  // private final String path;
-  // private final Class<? extends BaseEntity> associatedClass;
-
-  // private constructor
-  // private ResourcePath(String path, Class<? extends BaseEntity> clazz) {
-    // this.path = path;
-    // this.associatedClass = clazz;
-  // }
-
-  // public static ResourcePath forClass(Class<? extends BaseEntity> clazz) {
-    // for (ResourcePath rp : values()) {
-      // Cannot use equals because of hibernate proxied object
-      // Cannot use instanceof because type not fixed at compile time
-      // if (rp.associatedClass.isAssignableFrom(clazz)) {
-        // return rp;
-      // }
-    // }
-    // throw new IllegalArgumentException("No ResourcePath for class '" + clazz.getName() + "'");
-  // }
-
   public static String getPath(final String entityName) {
     String path = entityPathMap.get(entityName);
     System.out.println("### Mapped Path Entity : " + entityName + ", Path : "+ path);
     return path;
   }
 
-
-
-  // public Class<? extends BaseEntity> getAssociatedClass() {
-    // return associatedClass;
-  // }
 }
